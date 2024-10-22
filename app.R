ffe1e0292297397301b8765c11a43fc1dec3d9bd
nkcheung <nkcheung@ualberta.ca>
2024-10-22 10:50:56 -0600

    2 tabs launcher for clarity

--- app.R
+++ app.R
@@ -5,19 +5,29 @@ library(tcltk)
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("APD Analysis Launcher"),
+  titlePanel("FMD Brachial Tools Analysis Launcher"),
   
-  # Centered button layout
-  fluidRow(
-    column(12, align = "center",
-           actionButton("setwd_btn", "Set Working/Output Directory", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_mod", "FMD Analysis with viscosity model (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_mod_flip", "FMD Analysis with viscosity model (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_sp", "FMD Analysis with single-point viscosity (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_sp_flip", "FMD Analysis with single-point viscosity (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_no_v", "FMD Analysis without viscosity (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;"),
-           actionButton("fmd_no_v_flip", "FMD Analysis without viscosity (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;")
-           
+  # Tabs for organizing buttons
+  tabsetPanel(
+    tabPanel("Ch1 Flow, Ch2 Finger Pressure",
+             fluidRow(
+               column(12, align = "center",
+                      actionButton("setwd_btn", "Set Working/Output Directory", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_mod", "FMD Analysis with viscosity model (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_sp", "FMD Analysis with single-point viscosity (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_no_v", "FMD Analysis without viscosity (flow, finger pressure)", width = '800px', style = "font-size: 14px; margin: 20px;")
+               )
+             )
+    ),
+    tabPanel("Ch1 Finger Pressure, Ch2 Flow",
+             fluidRow(
+               column(12, align = "center",
+                      actionButton("setwd_btn", "Set Working/Output Directory", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_mod_flip", "FMD Analysis with viscosity model (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_sp_flip", "FMD Analysis with single-point viscosity (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;"),
+                      actionButton("fmd_no_v_flip", "FMD Analysis without viscosity (finger pressure, flow)", width = '800px', style = "font-size: 14px; margin: 20px;")
+               )
+             )
     )
   ),
   
@@ -59,7 +69,7 @@ server <- function(input, output, session) {
     busy(TRUE)
     status(NULL)
     output$loading_text <- renderText({ "Selecting Directory and Checking Permissions..." })
-
+    
     # Open directory selection dialog
     dir_path <- tryCatch({
       tk_choose.dir()
@@ -68,13 +78,13 @@ server <- function(input, output, session) {
       busy(FALSE)
       return(NULL)
     })
-
+    
     if (is.null(dir_path) || dir_path == "") {
       status("No directory selected.")
       busy(FALSE)
       return()
     }
-
+    
     # Create directory if it doesn't exist
     if (!dir.exists(dir_path)) {
       tryCatch({
@@ -85,7 +95,7 @@ server <- function(input, output, session) {
         return()
       })
     }
-
+    
     # Try to set the working directory
     tryCatch({
       setwd(dir_path)
@@ -98,10 +108,10 @@ server <- function(input, output, session) {
     }, error = function(e) {
       status(paste("Error setting working directory:", e$message))
     })
-
+    
     busy(FALSE)
   })
- 
+  
   observeEvent(input$fmd_mod, {
     busy(TRUE)
     status(NULL)
@@ -109,7 +119,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_mod_visc.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -119,6 +129,7 @@ server <- function(input, output, session) {
       busy(FALSE)
     }, delay = 2)  # Adjust delay as needed
   }) 
+  
   observeEvent(input$fmd_mod_flip, {
     busy(TRUE)
     status(NULL)
@@ -126,7 +137,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_mod_visc_FLIPPED.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -144,7 +155,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_sp_visc.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -154,6 +165,7 @@ server <- function(input, output, session) {
       busy(FALSE)
     }, delay = 2)  # Adjust delay as needed
   })
+  
   observeEvent(input$fmd_sp_flip, {
     busy(TRUE)
     status(NULL)
@@ -161,7 +173,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_sp_visc_FLIPPED.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -179,7 +191,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_mod_visc.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -197,7 +209,7 @@ server <- function(input, output, session) {
     
     # Simulate a script run with a delay
     later::later(function() {
-      # Try to source the  Analysis script
+      # Try to source the Analysis script
       tryCatch({
         source("https://github.com/nkcheung95/FMD-brachialtools-analyzer/blob/main/FMD_R_no_visc_FLIPPED.r?raw=TRUE")
         status("FMD Analysis Completed!")
@@ -207,8 +219,7 @@ server <- function(input, output, session) {
       busy(FALSE)
     }, delay = 2)  # Adjust delay as needed
   })
-  
+}
 
-} 
 # Run the application 
 shinyApp(ui = ui, server = server)
